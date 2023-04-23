ab4c3541b84b5c81d9ff44333a117c92b6e6b953
Gyro1203 <sebastian.vasquez2101@alumnos.ubiobio.cl>
2023-04-22 21:11:49 -0400

    avance ejercicios

---
+++ ejercicios1/parte2.js
@@ -0,0 +1,34 @@
+const numero1 = 10;
+const numero2 = 20;
+
+/* Opcion 1
+
+console.log(numero1 + numero2);
+
+console.log(numero1 - numero2);
+
+console.log(numero1 * numero2);
+
+console.log(numero1 / numero2);
+
+console.log(numero1 % numero2);
+
+*/
+
+//* Opcion 2
+
+const suma = numero1 + numero2;
+const resta = numero1 - numero2;
+const mult = numero1 * numero2;
+const div = numero1 / numero2;
+const mod = numero1 % numero2;
+
+console.log(suma);
+
+console.log(resta);
+
+console.log(mult);
+
+console.log(div);
+
+console.log(mod);
---
+++ ejercicios1/parte3.js
@@ -0,0 +1,23 @@
+let numero1 = 13;
+const numero2 = 15;
+const numero3 = '13';
+
+let dato = numero1;
+console.log("El numero 1 es: " + dato);
+
+
+if(numero1 == numero2) console.log("El primero y segundo numero son iguales");
+else if(numero1 > numero2) console.log("El primer numero es mayor que el segundo (" + numero1 + " > " + numero2 + ")");
+else console.log("El primer numero es menor que el segundo (" + numero1 + " < " + numero2 + ")");
+
+
+if(numero1 < numero3) console.log("El primer numero es menor que el tercero (" + numero1 + " < " + numero3 + ")");
+
+
+if(numero1 == numero3)console.log("El primero y tercer numero son iguales");
+
+if(numero1 != numero2)console.log("El primero y segundo numero son distintos");
+
+if(numero1 === numero3)console.log("El primero y tercer numero son estrictamente iguales");
+
+if(numero1 !== numero3)console.log("El primero y tercer numero son estrictamente diferentes");
