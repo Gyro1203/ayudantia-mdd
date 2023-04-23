9259f9bb14ae9e95f45eccc237ecd4c5332007ab
Gyro1203 <sebastian.vasquez2101@alumnos.ubiobio.cl>
2023-04-22 20:38:33 -0400

    Ejercicio 1

---
+++ ejercicios1/parte1.js
@@ -0,0 +1,12 @@
+let nombre = 'Sebastian';
+let edad = 20;
+let puedoConducir = false;
+
+console.log('Nombre: ' +  nombre);
+console.log('Tipo de dato: ' + typeof nombre);
+
+console.log('Edad: ' + edad);
+console.log('Tipo de dato: ' + typeof edad);
+
+console.log('¿Puede conducir?: ' + puedoConducir);
+console.log('Tipo de dato ' + typeof puedoConducir);
